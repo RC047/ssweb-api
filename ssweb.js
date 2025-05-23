fdc8e1b9bb759aee79fb6ca88002c919e7809207
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2025-05-23 22:17:11 +0700

    use supported chromium

--- ssweb.js
+++ ssweb.js
@@ -1,3 +1,4 @@
+let { headless, defaultViewport, args, executablePath } = require('@sparticuz/chromium')
 let puppeteer = require('puppeteer')
 let fetch = require('node-fetch')
 
@@ -15,23 +16,18 @@ function isURL(url) {
 module.exports = async function(url, options = {}) {
 	if (typeof url == 'undefined' || !isURL(url)) throw 'Invalid URL'
 	let browser = await puppeteer.launch({
-		headless: true,
-		defaultViewport: {
-			width: 1920,
-			height: 1080
-		},
-		args: [
-			'--no-sandbox',
-			'--disable-setuid-sandbox'
-		]
+		headless,
+		defaultViewport,
+		args,
+		executablePath: await executablePath()
 	})
 	let page = await browser.newPage()
 	let deviceType = options.deviceType || 'iPhone X'
 	if (!(deviceType in puppeteer.devices)) throw 'Device type is not supported!'
 	if (options.isMobile) await page.emulate(puppeteer.devices[deviceType])
 	await page.goto(url, { waitUntil: 'networkidle0' })
-    if (options.full) await page.waitForTimeout(15000)
+	if (options.full) await page.waitForTimeout(15000)
 	let buffer = await page.screenshot({ type: 'jpeg', quality: 100, fullPage: options.full })
-    await page.close()
+	await page.close()
 	return buffer
 }

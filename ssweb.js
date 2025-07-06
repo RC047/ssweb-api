6694990ee4b2224d42fddabdf22ae0a6841f2958
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2025-07-06 20:11:09 +0700

    Removed plugin

--- ssweb.js
+++ ssweb.js
@@ -1,7 +1,5 @@
 const { headless, defaultViewport, args, executablePath } = require('@sparticuz/chromium')
-const puppeteer = require('puppeteer-extra')
-const stealth = require('puppeteer-extra-plugin-stealth')
-const adblocker = require('puppeteer-extra-plugin-adblocker')
+const puppeteer = require('puppeteer')
 const fetch = require('node-fetch')
 
 
@@ -17,8 +15,6 @@ function isURL(url) {
 
 module.exports = async function(url, options = {}) {
 	if (typeof url == 'undefined' || !isURL(url)) throw 'Invalid URL'
-	puppeteer.use(stealth())
-	puppeteer.use(adblocker({ blockTrackers: true }))
 	let browser = await puppeteer.launch({
 		headless,
 		defaultViewport,
@@ -29,7 +25,7 @@ module.exports = async function(url, options = {}) {
 	let deviceType = options.deviceType || 'iPhone X'
 	if (!(deviceType in puppeteer.devices)) throw 'Device type is not supported!'
 	if (options.isMobile) await page.emulate(puppeteer.devices[deviceType])
-	// else await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
+	else await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
 	await page.goto(url, { waitUntil: 'networkidle0' })
 	if (options.full) await page.waitForTimeout(15000)
 	let buffer = await page.screenshot({ type: 'jpeg', quality: 100, fullPage: options.full })

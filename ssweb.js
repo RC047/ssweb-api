7e4689dea3078e329a07fa1cf81713c0f56ec5c9
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2025-07-06 22:12:39 +0700

    Update puppeteer

--- ssweb.js
+++ ssweb.js
@@ -23,8 +23,8 @@ module.exports = async function(url, options = {}) {
 	})
 	let page = await browser.newPage()
 	let deviceType = options.deviceType || 'iPhone X'
-	if (!(deviceType in puppeteer.devices)) throw 'Device type is not supported!'
-	if (options.isMobile) await page.emulate(puppeteer.devices[deviceType])
+	if (!(deviceType in puppeteer.KnownDevices)) throw 'Device type is not supported!'
+	if (options.isMobile) await page.emulate(puppeteer.KnownDevices[deviceType])
 	else await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36')
 	await page.goto(url, { waitUntil: 'networkidle0' })
 	if (options.full) await page.waitForTimeout(15000)
